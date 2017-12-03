79b37055f1a621eef9cda48d55c1918dc2900e6e
Prateek Hegde <prateekhegde12@gmail.com>
2017-12-03 22:35:42 +0530

    send location feature added

--- public/js/index.js
+++ public/js/index.js
@@ -16,14 +16,40 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a  = jQuery('<a target="_blank">My Current Location</a>');
+  li.text(`${message.from} `);
+  a.attr('href', message.url);
+  li.append(a);
+
+  jQuery('#messages').append(li);
+});
+
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
   socket.emit('createMessage', {
-    from: 'user',
+    from: 'User',
     text: jQuery('[name=message]').val()
   }, function(){
 
   });
+  $('[name=message]').val('');
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('geolocation is not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitute: position.coords.longitude
+    });
+  }, function(){
+    alert('unable to fetch location');
+  });
+})
