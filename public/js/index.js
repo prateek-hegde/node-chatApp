b64499fdffe5f96ad7ccf4d188bc2cf822510837
Prateek Hegde <prateekhegde12@gmail.com>
2017-12-03 23:15:57 +0530

    style is added to the chat page

--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,6 @@ socket.on('newLocationMessage', function (message) {
   li.text(`${message.from} `);
   a.attr('href', message.url);
   li.append(a);
-
   jQuery('#messages').append(li);
 });
 
@@ -30,13 +29,15 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(){
-
+    messageTextBox.val('');
   });
-  $('[name=message]').val('');
+
 });
 
 var locationButton = jQuery('#send-location');
@@ -44,12 +45,17 @@ locationButton.on('click', function(){
   if(!navigator.geolocation){
     return alert('geolocation is not supported by your browser');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Seding location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitute: position.coords.longitude
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('unable to fetch location');
   });
 })
