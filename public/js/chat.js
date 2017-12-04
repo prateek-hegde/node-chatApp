f3bb6275f55a83cb38559a15f91ea3b08ced7433
Prateek Hegde <prateekhegde12@gmail.com>
2017-12-04 10:00:55 +0530

    add user class and update userList

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,46 @@
 var socket = io();
 
 function scrollToBottom () {
-  //selectors
+  // Selectors
   var messages = jQuery('#messages');
-  var newMessage = messages.children('li:last-child');
-  //heights
+  var newMessage = messages.children('li:last-child')
+  // Heights
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
   var newMessageHeight = newMessage.innerHeight();
-  var lastMessageHEight = newMessage.prev().innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
-  if(clientHeight + scrollTop + newMessageHeight + lastMessageHEight >= scrollHeight){
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
   }
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
@@ -32,9 +51,9 @@ socket.on('newMessage', function (message) {
     from: message.from,
     createdAt: formattedTime
   });
+
   jQuery('#messages').append(html);
   scrollToBottom();
-
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -45,41 +64,40 @@ socket.on('newLocationMessage', function (message) {
     url: message.url,
     createdAt: formattedTime
   });
+
   jQuery('#messages').append(html);
   scrollToBottom();
 });
 
-
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  var messageTextBox = jQuery('[name=message]');
+  var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: messageTextBox.val()
-  }, function(){
-    messageTextBox.val('');
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
   });
-
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function(){
-  if(!navigator.geolocation){
-    return alert('geolocation is not supported by your browser');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr('disabled', 'disabled').text('Seding location...');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
     locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
-      longitute: position.coords.longitude
+      longitude: position.coords.longitude
     });
-  }, function(){
+  }, function () {
     locationButton.removeAttr('disabled').text('Send location');
-    alert('unable to fetch location');
+    alert('Unable to fetch location.');
   });
-})
+});
--- server/utils/message.js
+++ server/utils/message.js
@@ -6,14 +6,14 @@ var generateMessage = (from, text) => {
     text,
     createdAt: moment().valueOf()
   };
-}
+};
 
 var generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
-    url: `https://google.com/maps?q=${latitude},${longitude}`,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
     createdAt: moment().valueOf()
   };
-}
+};
 
 module.exports = {generateMessage, generateLocationMessage};
